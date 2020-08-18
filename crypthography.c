e35664ed8b81454f306acb6f251acf30134f900e
y0608 <54147006+y0608@users.noreply.github.com>
2020-08-18 21:42:27 +0300

    Fixed some bugs, added user interaction
    I can't do the scanf string!

--- crypthography.c
+++ crypthography.c
@@ -9,30 +9,47 @@ shifting(char* s, int shift)
 	{
 		if (*s >= 'a' && *s <= 'z' || *s >= 'A' && *s <= 'Z' || *s >= 'a' && *s <= 'z' || *s >= '0' && *s <= '9')//shifting only numbers and letters
 		{
-			if (*s + shift > 'z' || *s + shift > 'Z')
+			if (*s + shift > 'z')
 			{
-				if (*s + shift > 'z')
-				{
-					int toz = 'z' - *s;//spaces to z
-					*s = 'a' + toz;
-				}
-				if (*s + shift > 'Z' && *s + shift <'a')
-				{
-					int toz = 'Z' - *s;//spaces to Z
-					*s = 'A' + toz;
-				}
+				int toz = 'z' - *s;//spaces to z
+				*s = 'a' + shift - toz - 1;
+				s++;
 			}
-			*s += shift;//Shifting with x positions 
-			s++;//Going to next letter
+			else if (*s + shift > 'Z' && *s + shift <'a')
+			{
+				int toz = 'Z' - *s;//spaces to Z
+				*s = 'A' + shift - toz - 1;
+				s++;
+			}
+			else if (*s + shift > '9' && *s + shift < 'A')
+			{
+				int to9 = '9' - *s;//spaces to Z
+				*s = '0' + shift - to9 - 1;
+				s++;
+			}
+			else
+			{
+				*s += shift;//Shifting with x positions 
+				s++;//Going to next letter
+			}
+			
 		}
 		else
 			s++;
 	}
 }
 
-void main()
+void main(int argc,const char *argv[])
 {
-	char str[100] = "abcdefg";
-	int shift = 1;//shifting with x positions
+	int shift;
+	char str[100];
+	printf("Enter your code:\n");
+	scanf_s("%s", str);
+	do
+	{
+		printf("Enter a number between 0 and 26:\n");
+		scanf_s("%d", &shift);
+	} while (shift > 25 || shift < 0);
 	shifting(str, shift);
+	printf("%s\n",str);
 }
