2b50f8c55b658b3b589dcd7f6230d1b1b7cdd179
Yoan Ivanov <54147006+y0608@users.noreply.github.com>
2022-05-14 13:45:41 +0300

    Create ceaser.c

---
+++ ceaser.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *encypher(char *data, int shift)
+{
+    char *result = malloc(sizeof(char) * strlen(data));
+
+    for (int i = 0; i < strlen(data); i++)
+    {
+        if (data[i] < 'a' || data[i] > 'z')
+            result[i] = data[i];
+        else if (data[i] < 'z' - shift)
+            result[i] = data[i] + shift;
+        else
+            result[i] = data[i] - ('z' - 'a') + shift;
+    }
+
+    return result;
+}
+
+char *decypher(char *data, int shift)
+{
+    char *result = malloc(sizeof(char) * strlen(data));
+
+    for (int i = 0; i < strlen(data); i++)
+    {
+        if (data[i] < 'a' || data[i] > 'z')
+            result[i] = data[i];
+        else if (data[i] - shift > 'a')
+            result[i] = data[i] - shift;
+        else
+            result[i] = data[i] + ('z' - 'a') - shift;
+    }
+
+    return result;
+}
+
+void decode(char *encrypted)
+{
+    printf("\nDecrypting...\n");
+    for (int i = 1; i < 25; i++)
+    {
+        char *decrypted = decypher(encrypted, i);
+        printf("%d: %s\n", i, decrypted);
+    }
+}
+
+int main()
+{
+    char *encrypted = encypher("hello", 10);
+    printf("%s\n", encrypted);
+    decode(encrypted);
+    return 0;
+}
