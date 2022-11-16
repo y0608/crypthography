8056d0b6c6e11a1131860f690aafa3f2ba2f6410
Yoan Laptop <yoanivanov9@gmail.com>
2022-11-16 11:21:39 +0200

    convert cpp to c

--- sha/sha256.c
+++ sha/sha256.c
@@ -1,84 +1,156 @@
-#define _CRT_SECURE_NO_WARNINGS
-// #include "operations.h"
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdint-gcc.h>
+#include "hash.h"
 
-/*CONSTANTS*/
-unsigned int constants[64] = { 0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
-0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
-0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
-0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
-0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
-0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
-0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
-0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2 };
-
-/*OPERATIONS*/
-#define ROTLEFT(a,b) (((a) << (b)) | ((a) >> (32-(b))))
-#define ROTRIGHT(a,b) (((a) >> (b)) | ((a) << (32-(b))))
-#define CH(x,y,z) (((x) & (y)) ^ (~(x) & (z)))
-#define MAJ(x,y,z) (((x) & (y)) ^ ((x) & (z)) ^ ((y) & (z)))
-#define s0(x) (ROTRIGHT(x,7) ^ ROTRIGHT(x,18) ^ ((x) >> 3))
-#define s1(x) (ROTRIGHT(x,17) ^ ROTRIGHT(x,19) ^ ((x) >> 10))
-#define S0(x) (ROTRIGHT(x,2) ^ ROTRIGHT(x,13) ^ ROTRIGHT(x,22))
-#define S1(x) (ROTRIGHT(x,6) ^ ROTRIGHT(x,11) ^ ROTRIGHT(x,25))
-
-/*PRINT*/
-void print_binary_number(int n)
+void print_binary_number(uint8_t n)
 {
-    for (int c = 7; c >= 0; c--)
+    for (int i = 0; i < 8; i++)
     {
-        if (n >> c & 1)
+        if (n & 0x80)
             printf("1");
         else
             printf("0");
+        n <<= 1;
     }
     printf("-");
 }
-void print(char* input, int size)
+
+void print_binary_number32(uint32_t n)
 {
-    for (int i = 0; i < size; i++)
+    for (int i = 0; i < 32; i++)
     {
-        print_binary_number(*input);
-        input++;
+        if (i % 8 == 0)
+            printf(":");
+        if (n & 0x80000000)
+            printf("1");
+        else
+            printf("0");
+        n <<= 1;
     }
+    printf("-");
 }
 
-int main()
+void print(uint8_t *input, int size)
 {
-    char* input_string = "abc";
-    int input_len = strlen(input_string),
-        blocks = input_len/64;
+    for (int i = 0; i < size; i++)
+        print_binary_number(input[i]);
+    printf("\n");
+}
 
-    //512 bits
-    char *block = malloc(64);
-    if (!block) return 0;
-    memset(block, 0, 64);
+int main(int argc, char **argv)
+{
+    uint32_t initial_hash[] = {0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a, 0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19};
 
-    char null = 128;
+    // char input[] = "abc";
+    char *input = argv[1];
 
-    for (int i = 0; i <= blocks; i++)
+    uint64_t input_length = strlen(input);
+    // cout << input_length << endl;
+
+    // 448 bits(56 bytes) of info and 64(8 bytes) for length of message in the last block
+    int number_of_blocks = (input_length + 8) / 64 + 1;
+
+    for (int block_number = 0; block_number < number_of_blocks; block_number++)
     {
-        if(input_len>=56)
-            memcpy(block, input_string, 56);
-        else
+        int end_of_block = block_number * 64 + 64;
+
+        // Copy input into block
+        uint8_t block[64] = {0};
+        int input_index = -1, block_index = -1;
+        for (input_index = end_of_block - 64, block_index = 0;
+             input_index < end_of_block && input_index < input_length;
+             input_index++, block_index++)
+            block[block_index] = input[input_index];
+
+        // Last block
+        if (block_number == number_of_blocks - 1)
         {
-            memcpy(block, input_string, input_len);
-            memcpy(block+input_len, &null, sizeof(null));
+            // Add 1 between msg and padded 0s
+            block[input_length % 64] = 128;
+
+            //  Padd with 0s until 56th byte - already padded because block is full of 0s at the start
+
+            // Add the length of the message in the last 64 bits(8 bytes)
+            int bit_length = input_length * 8;
+            for (int i = 0; i < 8; i++)
+            {
+                block[63 - i] = bit_length & 255; // 0b11111111
+                bit_length >>= 8;
+            }
         }
-        unsigned long long len = 1;
+        else if (block_index != end_of_block)
+            block[block_index] = 128;
 
-        memcpy(block + 56, &len, 8);
+        // Create message scheudle:
+        uint32_t msg_schedule[64];
+        uint32_t current_word = 0;
 
-        print(block,64);
-        //hash
-       
-        input_string += 64;
-        input_len -= 64;
+        // Add the first 16 values from block
+        int msg_schedule_index = 0;
+        for (int i = 0; i < 64; i += 4)
+        {
+            // block is uint8 and msg_schedule is uint32
+            // put 4 bytes from block to one msg_schedule
+            for (int j = 0; j < 3; j++)
+            {
+                current_word |= block[i + j];
+                current_word <<= 8;
+            }
+            // no need to <<= 8
+            current_word |= block[i + 3];
 
-        memset(block, 0, 64);
+            msg_schedule[msg_schedule_index] = current_word;
+
+            msg_schedule_index++;
+            current_word = 0;
+        }
+        // msg_schedule_index is now 16
+
+        // Continue making the message schedule
+        for (; msg_schedule_index < 64; msg_schedule_index++)
+            msg_schedule[msg_schedule_index] = s1(msg_schedule[msg_schedule_index - 2]) + msg_schedule[msg_schedule_index - 7] + s0(msg_schedule[msg_schedule_index - 15]) + msg_schedule[msg_schedule_index - 16];
+
+        // COPMRESSION:
+
+        // copy initial hash
+        uint32_t state_registers[8];
+        for (int i = 0; i < 8; i++)
+            state_registers[i] = initial_hash[i];
+
+        // traverse every word from schedule
+        for (int i = 0; i < 64; i++)
+        {
+            // calculate temprary varaibles
+            uint32_t tmp1 = S1(state_registers[4]) + CH(state_registers[4], state_registers[5], state_registers[6]) + state_registers[7] + constants[i] + msg_schedule[i],
+                     tmp2 = S0(state_registers[0]) + MAJ(state_registers[0], state_registers[1], state_registers[2]);
+
+            // move everything down
+            state_registers[7] = state_registers[6];
+            state_registers[6] = state_registers[5];
+            state_registers[5] = state_registers[4];
+            state_registers[4] = state_registers[3];
+            state_registers[3] = state_registers[2];
+            state_registers[2] = state_registers[1];
+            state_registers[1] = state_registers[0];
+
+            state_registers[0] = tmp1 + tmp2;
+
+            state_registers[4] += tmp1;
+        }
+
+        // UPDATE STATE REGISTERS
+        for (int i = 0; i < 8; i++)
+        {
+            // combine state registers and initial hash
+            state_registers[i] += initial_hash[i];
+            // update initial_hash
+            initial_hash[i] = state_registers[i];
+        }
     }
 
-    return 0;
+    // print
+    for (int i = 0; i < 8; i++)
+        printf("%04x", initial_hash[i]);
+    printf("\n");
 }
